66efff9fda918d86a4865b26b49c51140be58e74
Mike McGowan <mike.mcgowan@leolearning.com>
2018-08-31 14:37:30 +0100

    Replaced Java with Scala

---
+++ src/main/scala/hello/HelloController.scala
@@ -0,0 +1,13 @@
+package hello
+
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+
+@RestController
+class HelloController {
+
+    @RequestMapping(Array("/"))
+    def index(): String = "Greetings from Spring Boot!"
+
+}
+
