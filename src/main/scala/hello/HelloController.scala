c7db533fb6e57a21de068f06808325767cba1f14
Mike McGowan <mike.mcgowan@leolearning.com>
2018-08-31 15:36:27 +0100

    Whitespace

--- src/main/scala/hello/HelloController.scala
+++ src/main/scala/hello/HelloController.scala
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.{RequestMapping, RestController}
 @RestController
 class HelloController {
 
-    @RequestMapping(Array("/"))
-    def index(): String = "Greetings from Spring Boot!"
+  @RequestMapping(Array("/"))
+  def index(): String = "Greetings from Spring Boot!"
 
 }
