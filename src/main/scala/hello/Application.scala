b15cf44e73879888154651283317dc391e403a70
Mike McGowan <mike.mcgowan@leolearning.com>
2018-08-31 15:35:42 +0100

    Added command line runner to show provided beans

--- src/main/scala/hello/Application.scala
+++ src/main/scala/hello/Application.scala
@@ -1,7 +1,9 @@
 package hello
 
-import org.springframework.boot.SpringApplication
+import org.springframework.boot.{CommandLineRunner, SpringApplication}
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class Application {}
@@ -11,5 +13,12 @@ object Application {
   def main(args: Array[String]): Unit = {
     SpringApplication.run(classOf[Application], args: _*)
   }
+
+  @Bean
+  def commandLineRunner(ctx: ApplicationContext): CommandLineRunner =
+    (_: Array[String]) => {
+      println("Let's inspect the beans provided by Spring Boot:")
+      ctx.getBeanDefinitionNames.sorted.foreach(println)
+    }
 
 }
