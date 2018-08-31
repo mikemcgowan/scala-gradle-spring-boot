cbb3eee18ab635536777fbfccd98abc8e994643b
Mike McGowan <mike.mcgowan@leolearning.com>
2018-08-31 15:02:17 +0100

    Imports

--- src/main/scala/hello/HelloController.scala
+++ src/main/scala/hello/HelloController.scala
@@ -1,7 +1,6 @@
 package hello
 
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.{RequestMapping, RestController}
 
 @RestController
 class HelloController {
